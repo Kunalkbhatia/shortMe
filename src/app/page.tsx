bd905c2c50177199d625d9ff68e3f32b0831375a
Kunalkbhatia <bhatiakbkb@gmail.com>
2024-11-02 17:43:13 +0530

    minor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,42 +31,6 @@ async function Dashboard() {
   });
 
   return (
-    // <div className="flex flex-col gap-2 border-4 border-red-900">
-    //   <header className="flex justify-between items-center h-[60px] p-8">
-    //     <h1 className="text-3xl font-semibold">ShortMe</h1>
-    //     <div className="flex gap-4">
-    //       <div className="flex items-center gap-4">
-    //         <ManageTags tags={tags} />
-    //         <CreateFolder />
-    //         <CreateURL tags={tags} />
-    //       </div>
-    //       <Profile />
-    //     </div>
-    //   </header>
-    //   <main className="flex-1 grid lg:grid-cols-3 gap-4 p-4 overflow-hidden">
-    //     <div className="lg:col-span-2 flex flex-col gap-4  overflow-hidden">
-    //       <div className="flex justify-between gap-4">
-    //         <Input
-    //           placeholder="Search with Long URL"
-    //           className="w-[50%] focus-within:outline-none bg-gray-200"
-    //         />
-    //         <div className="flex gap-2">
-    //           <SelectFolder />
-    //           <SelectFolder />
-    //         </div>
-    //       </div>
-    //       <div className="flex-1 grid md:grid-cols-2 gap-2 overflow-y-auto  ">
-    //         {urls.map((url) => (
-    //           <URLCard url={url} key={url.id} />
-    //         ))}
-    //       </div>
-    //     </div>
-    //     <div className="hidden lg:block lg:col-span-1 overflow-auto space-y-5">
-    //       <Chart />
-    //       <Chart />
-    //     </div>
-    //   </main>
-    // </div>
     <div className="flex-1 flex flex-col space-y-12 p-8 h-screen overflow-hidden">
       <div className="text-3xl font-bold text-prim">Links</div>
       <div className="grid lg:grid-cols-2 gap-5">
