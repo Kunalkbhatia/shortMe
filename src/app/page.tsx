1d0a9be896a8423670ab3a993820e129b736b09f
Kunalkbhatia <bhatiakbkb@gmail.com>
2024-11-12 20:27:17 +0530

    landing page made reponsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,44 +11,44 @@ import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 function Landing() {
   return (
-    <div className="p-11 space-y-20">
+    <div className=" p-5 md:p-11 space-y-20">
       <div className="flex justify-between items-center">
         <div className="font-bold text-4xl">Short Me</div>
         <Link
           href="/sign-in"
-          className="font-semibold bg-primaryButton text-white text-xl p-4 rounded-full"
+          className="font-semibold bg-primaryButton text-white text-xl p-2 md:p-4 rounded-full"
         >
           Get Started
         </Link>
       </div>
-      <div className="flex flex-col gap-5 items-center bg-secondaryBg py-10 rounded-full">
+      <div className="flex flex-col gap-5 items-center bg-secondaryBg p-4 rounded-xl">
         <div className="bg-white p-3 rounded-full font-semibold">
           🔥 Introducing ShortMe
         </div>
-        <div className="font-extrabold text-5xl text-center w-[700px]">
+        <div className="font-extrabold text-3xl md:text-5xl lg:text-7xl text-center lg:w-[900px]">
           URL shortening amplified, where insights meet impact.
         </div>
-        <div className="font-semibold text-gray-500 w-[600px] text-center">
+        <div className="font-semibold text-gray-500 md:text-2xl md:w-[600px] text-center">
           Elevate your brand presence with ShortMe : An URL shortening for
           impactful digital trails. Seamlessly boost brand recognition
         </div>
         <Button className="bg-primaryButton ">Watch Demo</Button>
         <video
-          width={500}
+          width={800}
           height={500}
           src="https://www.youtube.com/watch?v=38fhj9tFfAQ"
           className="border-4 border-black rounded-md"
         />
       </div>
-      <div className="flex flex-col gap-8 items-center bg-secondaryBg py-10 rounded-full">
+      <div className="flex flex-col gap-8 items-center bg-secondaryBg py-10 rounded-xl">
         <div className="bg-white p-3 rounded-full font-semibold">
           🔥 The Problem
         </div>
-        <div className="font-extrabold text-5xl text-center w-[700px]">
+        <div className="font-extrabold text-3xl md:text-5xl text-center md:w-[700px]">
           All-in-One Solution for Link Management
         </div>
 
-        <div className="grid grid-cols-3 gap-8 px-32">
+        <div className="grid md:grid-cols-3 gap-8 p-10">
           <div>
             <Image src={svg1} width={50} height={50} alt="svg1" />
             <div>
@@ -86,10 +86,10 @@ function Landing() {
         <div className="bg-secondaryBg p-3 rounded-full font-semibold">
           🔥 The Solution
         </div>
-        <div className="font-extrabold text-4xl text-center w-[700px]">
+        <div className="font-extrabold text-3xl md:text-4xl text-center md:w-[700px]">
           Effortlessly Scale Your Marketing Efforts
         </div>
-        <div className="grid grid-cols-2 gap-10">
+        <div className="grid md:grid-cols-2 gap-10">
           <Card className="flex flex-col items-center">
             <CardHeader>
               <Image src={chart} width={500} alt="chart" />
@@ -124,10 +124,10 @@ function Landing() {
           </Card>
         </div>
       </div>
-      <div className="flex justify-around gap-5 items-center bg-secondaryBg py-10 rounded-full">
+      <div className="flex justify-around gap-5 items-center bg-secondaryBg p-5 rounded-xl">
         <div>
           <div className="text-2xl font-bold">ShortMe</div>
-          <div className="text-xl">
+          <div className="md:text-xl">
             Increase productivity and achieve better results
           </div>
           <div className="font-extralight text-gray-400">
