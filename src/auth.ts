5990da157cdb758f9ead8883b9051a8ff3a56712
Kunalkbhatia <bhatiakbkb@gmail.com>
2024-11-24 14:02:36 +0530

    google auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,6 @@
-import NextAuth, { CredentialsSignin } from "next-auth";
+import NextAuth, { AuthError, CredentialsSignin } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 import prisma from "./lib/db";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
@@ -20,9 +21,41 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
     jwt: async({token}) => {
       return token;
+    },
+    signIn: async ({user, account}) => {
+      if(account?.provider === "google"){
+        try {
+          const {email, name, image, id} = user;
+          const exist = prisma.user.findUnique({
+            where: {
+              email: email ?? ""
+            }
+          });
+          if(!exist) {
+            await prisma.user.create({
+              data: {
+                username: name,
+                email,
+                googleId: id,
+                image,  
+              }
+            })
+          }
+          else return true;
+        } catch (error: unknown) {
+          console.log(error);
+          throw new AuthError("Erro while creating account");
+        }
+      }
+      if(account?.provider === "credentials") return true;
+      return false;
     }
   },
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     Credentials({
       name: "Credentials",
       credentials: {
--- src/components/Forms/Sign-up/index.tsx
+++ src/components/Forms/Sign-up/index.tsx
@@ -5,67 +5,72 @@ import { Button } from "@/components/ui/button";
 import { redirect } from "next/navigation";
 import { createUser } from "@/actions/createUser";
 import { useToast } from "@/hooks/use-toast";
+import { loginWithGoogle } from "@/actions/loginUser";
 
 const SignUpForm = () => {
   const { toast } = useToast();
   return (
-    <form
-      action={async (formData: FormData) => {
-        const error = await createUser(formData);
-        if (error) {
-          toast({
-            title: error,
-            variant: "destructive",
-          });
-        } else {
-          toast({
-            title: "Account created successfully",
-            description: "Please Login to continue",
-          });
-          redirect("/sign-in");
-        }
-      }}
-    >
-      <div className="grid gap-4">
-        <div className="grid gap-2">
-          <Label htmlFor="first-name">Username</Label>
-          <Input
-            type="text"
-            name="username"
-            placeholder="durdenTaylor"
-            className="border-2 border-b-4 border-primaryBorder focus-within:outline-none"
-          />
+    <div className="space-y-3">
+      <form
+        action={async (formData: FormData) => {
+          const error = await createUser(formData);
+          if (error) {
+            toast({
+              title: error,
+              variant: "destructive",
+            });
+          } else {
+            toast({
+              title: "Account created successfully",
+              description: "Please Login to continue",
+            });
+            redirect("/sign-in");
+          }
+        }}
+      >
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="first-name">Username</Label>
+            <Input
+              type="text"
+              name="username"
+              placeholder="durdenTaylor"
+              className="border-2 border-b-4 border-primaryBorder focus-within:outline-none"
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              type="email"
+              name="email"
+              placeholder="mayhen@taylor.com"
+              className="border-2 border-b-4 border-primaryBorder focus-within:outline-none"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              type="password"
+              name="password"
+              placeholder="************"
+              className="border-2 border-b-4 border-primaryBorder focus-within:outline-none"
+            />
+          </div>
+          <Button
+            type="submit"
+            className="w-full bg-primaryButton hover:bg-hoverButton"
+          >
+            Create an account
+          </Button>
         </div>
-        <div className="grid gap-2">
-          <Label htmlFor="email">Email</Label>
-          <Input
-            type="email"
-            name="email"
-            placeholder="mayhen@taylor.com"
-            className="border-2 border-b-4 border-primaryBorder focus-within:outline-none"
-            required
-          />
-        </div>
-        <div className="grid gap-2">
-          <Label htmlFor="password">Password</Label>
-          <Input
-            type="password"
-            name="password"
-            placeholder="************"
-            className="border-2 border-b-4 border-primaryBorder focus-within:outline-none"
-          />
-        </div>
-        <Button
-          type="submit"
-          className="w-full bg-primaryButton hover:bg-hoverButton"
-        >
-          Create an account
-        </Button>
-        <Button variant="outline" className="w-full">
+      </form>
+      <form action={loginWithGoogle}>
+        <Button variant="outline" type="submit" className="w-full">
           Sign up with Google
         </Button>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
 
